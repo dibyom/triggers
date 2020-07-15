63b8762bb053cb757be57de503abc868dd7c3a13
Meixing Dong <meixingdong@google.com>
2020-07-15 20:48:35 +0000

    Add ProcessTriggerSpec function to take a Triggers.yaml file, find all referenced bindings, find the trigger template, apply the HTTP request to the binding and template

--- cmd/triggerRun/cmd/root.go
+++ cmd/triggerRun/cmd/root.go
@@ -18,21 +18,31 @@ package cmd
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 
 	"github.com/spf13/cobra"
 	"github.com/tektoncd/triggers/pkg/apis/triggers/v1alpha1"
+	triggersv1 "github.com/tektoncd/triggers/pkg/apis/triggers/v1alpha1"
+	"go.uber.org/zap"
+
+	V1alpha1Client "github.com/tektoncd/triggers/pkg/client/clientset/versioned/typed/triggers/v1alpha1"
+	"github.com/tektoncd/triggers/pkg/template"
 	"k8s.io/apimachinery/pkg/runtime/serializer/streaming"
 	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 var (
-	rootCmd = &cobra.Command{
+	kubeconfig string
+	rootCmd    = &cobra.Command{
 		Use:   "triggers-run",
-		Short: "Tekton Trigger test",
+		Short: "This is the CLI for tekton trigger",
+		Long:  "tkn-trigger will allow you",
 		Run:   rootRun,
 	}
 
@@ -43,10 +53,71 @@ var (
 func init() {
 	rootCmd.Flags().StringVarP(&triggerFile, "triggerFile", "t", "", "Path to trigger yaml file")
 	rootCmd.Flags().StringVarP(&httpPath, "httpPath", "r", "", "Path to body event")
+	rootCmd.PersistentFlags().StringVarP(&kubeconfig, "kubeconfig", "k", "", "absolute path to the kubeconfig file")
 }
 
 func rootRun(cmd *cobra.Command, args []string) {
 	// TODO: Not implemented
+	// client, err := GetKubeClient("kubeClient")
+
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println(client)
+
+}
+
+func trigger(w io.Writer, triggerFile, httpPath string) error {
+	// Read HTTP request.
+	r, err := readHTTP(httpPath)
+	if err != nil {
+		return fmt.Errorf("error reading HTTP file: %w", err)
+	}
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return fmt.Errorf("error reading HTTP body: %w", err)
+	}
+
+	// Read triggers.
+	triggers, err := readTrigger(triggerFile)
+	if err != nil {
+		return fmt.Errorf("error reading triggers: %w", err)
+	}
+
+	t := template.ResolvedTrigger{
+		// Triggers: triggers,
+	}
+
+	fmt.Printf("%+v", t)
+
+	fmt.Printf("%+v", r.Header)
+	fmt.Println("-------------")
+	fmt.Printf("%+v", r.Body)
+	fmt.Println("-------------")
+	fmt.Printf("%+v", body)
+	fmt.Printf("%+v", triggers)
+
+	// ResolveParams's alternative
+	// https://github.com/tektoncd/triggers/blob/69e02ea03064bb08b7b53638f3c2a1ef9508d5cf/pkg/sink/sink.go#L150
+
+	// params, err := template.ResolveParams(t, body, r.Header)
+	// if err != nil {
+	//  return fmt.Errorf("error resolving params: %w", err)
+	// }
+
+	// // Sort results for stable output.
+	// sort.SliceStable(params, func(i, j int) bool {
+	//  return params[i].Name < params[j].Name
+	// })
+
+	// enc := json.NewEncoder(w)
+	// enc.SetIndent("", "  ")
+	// if err := enc.Encode(params); err != nil {
+	//  return fmt.Errorf("error encoding params: %w", err)
+	// }
+
+	return nil
 }
 
 func readTrigger(path string) ([]*v1alpha1.Trigger, error) {
@@ -85,6 +156,66 @@ func readHTTP(path string) (*http.Request, error) {
 	defer f.Close()
 
 	return http.ReadRequest(bufio.NewReader(f))
+}
+
+func GetKubeClient(kubeconfig string) (*V1alpha1Client.TriggersV1alpha1Client, error) {
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		return nil, fmt.Errorf("fail to build config from the flags: %w", err)
+	}
+
+	return V1alpha1Client.NewForConfig(config)
+}
+
+func processTriggerSpec(t *triggersv1.TriggerSpec, request *http.Request, event []byte, eventID string, eventLog *zap.SugaredLogger) error {
+	client, err := GetKubeClient("kubeClient")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(client)
+
+	if t == nil {
+		return errors.New("EventListenerTrigger not defined")
+	}
+
+	log := eventLog.With(zap.String(triggersv1.TriggerLabelKey, t.Name))
+	fmt.Print(log)
+
+	// finalPayload, header, err := client.executeInterceptors(t, request, event, log)
+	// if err != nil {
+	// 	log.Error(err)
+	// 	return err
+	// }
+
+	// rt, err := template.ResolveTrigger(*t,
+	// 	r.TriggersClient.TriggersV1alpha1().TriggerBindings(r.EventListenerNamespace).Get,
+	// 	r.TriggersClient.TriggersV1alpha1().ClusterTriggerBindings().Get,
+	// 	r.TriggersClient.TriggersV1alpha1().TriggerTemplates(r.EventListenerNamespace).Get)
+	// if err != nil {
+	// 	log.Error(err)
+	// 	return err
+	// }
+
+	// params, err := template.ResolveParams(rt, finalPayload, header)
+	// if err != nil {
+	// 	log.Error(err)
+	// 	return err
+	// }
+
+	// log.Infof("ResolvedParams : %+v", params)
+	// resources := template.ResolveResources(rt.TriggerTemplate, params)
+	// token, err := r.retrieveAuthToken(t.ServiceAccount, eventLog)
+	// if err != nil {
+	// 	log.Error(err)
+	// 	return err
+	// }
+	// if err := r.createResources(token, resources, t.Name, eventID, log); err != nil {
+	// 	log.Error(err)
+	// 	return err
+	// }
+	return nil
 }
 
 // Execute runs the command.
