1b31e1115d238c85d6f20cd2bda67fa21f44c97e
Dibyo Mukherjee <dibyo@google.com>
2020-10-26 12:20:06 -0400

    versioning + tlsCerts

--- pkg/apis/triggers/v1alpha1/interceptor_types.go
+++ pkg/apis/triggers/v1alpha1/interceptor_types.go
@@ -8,7 +8,7 @@ import (
 )
 
 type InterceptorInterface interface {
-	Process(ctx context.Context,  *InterceptorRequest) *InterceptorResponse
+	Process(ctx context.Context, r *InterceptorRequest) *InterceptorResponse
 }
 
 type InterceptorRequest struct {
@@ -24,8 +24,8 @@ type InterceptorRequest struct {
 }
 
 type TriggerContext struct {
-	// EventUrl is the URL of the incoming event
-	EventUrl string `json:"url,omitempty"`
+	// EventURL is the URL of the incoming event
+	EventURL string `json:"url,omitempty"`
 	// EventID is a unique ID assigned by Triggers to each event
 	EventID string `json:"event_id,omitempty"`
 	// TriggerID is of the form namespace/$ns/triggers/$name
@@ -49,4 +49,57 @@ func ParseTriggerID(triggerID string) (namespace, name string) {
 	}
 
 	return splits[1], splits[3]
+}
+
+type InterceptorConfigurationSpec struct {
+	// ClientConfig defines how to communicate with the interceptor service
+	// Required
+	ClientConfig InterceptorClientConfig `json:"clientConfig"`
+
+	// Params declare interceptor specific fields thatthe user can configure per Trigger.
+	Params []InterceptorParamSpec `json:"params"`
+}
+
+type InterceptorClientConfig struct {
+	// `url` gives the location of the webhook, in standard URL form
+	// (`scheme://host:port/path`). Exactly one of `url` or `service`
+	// must be specified.
+	// +optional
+	URL *string `json:"url,omitempty"`
+
+	// `service` is a reference to the service for this webhook. Either
+	// `service` or `url` must be specified.
+	// +optional
+	Service *ServiceReference `json:"service,omitempty"`
+
+	// `caBundle` is a PEM encoded CA bundle which will be used to validate the webhook's server certificate.
+	// +optional
+	CABundle []byte `json:"caBundle,omitempty"`
+}
+
+type InterceptorParamSpec struct {
+	// Name is the name of the param field
+	Name string `json:"name"`
+	// Optional is true if the field is optional
+	Optional bool `json:"optional"`
+}
+
+type ServiceReference struct {
+	// `namespace` is the namespace of the service.
+	// Required
+	Namespace string `json:"namespace" protobuf:"bytes,1,opt,name=namespace"`
+	// `name` is the name of the service.
+	// Required
+	Name string `json:"name" protobuf:"bytes,2,opt,name=name"`
+
+	// `path` is an optional URL path which will be sent in any request to
+	// this service.
+	// +optional
+	Path *string `json:"path,omitempty" protobuf:"bytes,3,opt,name=path"`
+
+	// If specified, the port on the service that hosting webhook.
+	// Default to 443 for backward compatibility.
+	// `port` should be a valid port number (1-65535, inclusive).
+	// +optional
+	Port *int32 `json:"port,omitempty" protobuf:"varint,4,opt,name=port"`
 }
